03b7ed2b72dca4bfed9c0ff7d248824a955726ff
woo <woo@wooos-MacBook-Pro.local>
2017-10-17 17:13:27 +0800

    1.汉化, 2.增加自定义图片上传组件, 3.增加自定义视频上传组件, 控制编辑器默认高度100%

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,105 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <tinymce id="d1" v-model="data"></tinymce>
-    <button type="button" @click="clickHandler">Reset</button>
-    <div>{{ data }}</div>
-  </div>
+    <div id="app">
+        <!-- <img src="./assets/logo.png"> -->
+        <tinymce v-model="data" :uploadImage='uploadImage' style="min-height: 400px;"></tinymce>
+        <button type="button" @click="clickHandler" style='margin-top: 20px;'>Reset</button>
+        <div>{{ data }}</div>
+    </div>
 </template>
 
 <script>
 import tinymce from './components/TinymceVue'
+import axios from 'axios'
+import crypto from 'crypto'
+
+const MD5 = (str) => {
+    const hash = crypto.createHash('md5')
+    hash.update(str)
+    return hash.digest('hex').toString()
+}
 
 export default {
-  name: 'app',
-  components: {
-    tinymce
-  },
-  data(){
-      return {
-          data : '<p>Hello from tinymce</p>'
-      };
-  },
-  mounted(){
-     
-  },
-  methods: {
-      clickHandler(){
-          this.data = 'You reseted tinymce\'s content';
-      }
-  }
+    name: 'app',
+    components: {
+        tinymce
+    },
+    data() {
+        return {
+            data: '',
+            uploadImage: async (file) => {
+                const getEliveToken = () => {
+                    return new Promise((resolve, reject) => {
+                        axios.get('http://10.0.60.95:7997/getEliveToken').then((data) => {
+                            if (data.body.code === 1) {
+                                resolve({ ts: data.body.data.ts, token: data.body.data.ltoken })
+                            } else {
+                                alert(data.body.msg + ', code:' + data.body.code)
+                                reject(data.body.msg)
+                            }
+                        }).catch(function(data) {
+                            if (data.body) {
+                                alert(data.body.msg + ', code:' + data.body.code)
+                                reject(data.body.msg)
+                            } else {
+                                alert('获取eliveToken失败, 网络异常')
+                                reject('获取eliveToken失败, 网络异常')
+                            }
+                        })
+                    })
+                }
+                const ts = (new Date()).getTime()
+                const token = MD5('e02a4a4be3afd8899ff79898803d00cc' + ts)
+                const uploadImg = () => {
+                    return new Promise((resolve, reject) => {
+                        const postData = new FormData()
+                        postData.append('bizType', 5)
+                        postData.append('file', file)
+                        axios.post('http://10.0.60.95:8300/qiniu/image/single', postData, {
+                            headers: {
+                                'Content-Type': 'multipart/form-data',
+                                'ts': ts.toString(),
+                                'token': token
+                            }
+                        }).then(response => {
+                            if (response.data.code === 1) {
+                                resolve(response.data.data.url)
+                            } else {
+                                alert(response.data.msg)
+                                reject(response.data.msg)
+                            }
+                        }, response => {
+                            if (response.data.msg) {
+                                alert(response.data.msg)
+                                reject(response.data.msg)
+                            } else {
+                                alert('网络异常, 上传图片失败')
+                                reject('网络异常, 上传图片失败')
+                            }
+                        })
+                    })
+                }
+                return await uploadImg()
+            }
+        };
+    },
+    mounted() {
+
+    },
+    methods: {
+        clickHandler() {
+            this.data = '';
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 60px; */
 }
 </style>
--- src/components/TinymceVue.vue
+++ src/components/TinymceVue.vue
@@ -1,162 +1,223 @@
 <template>
-  <div>
-      <textarea :id="id">{{ content }}</textarea>
-  </div>
+    <div :id='randomID'>
+        <textarea :id="editorID" v-model="content"></textarea>
+        <ImageManager ref='imageManager' :uploadImage='uploadImage' />
+        <VideoManager ref='videoManager' />
+    </div>
 </template>
 
 <script>
-   // Import TinyMCE
-    import tinymce from 'tinymce/tinymce';
+// 导入tinymce
+import tinymce from 'tinymce/tinymce.min'
 
-    // A theme is also required
-    import 'tinymce/themes/modern/theme';
+// theme, 必须导入, 否则报错
+import 'tinymce/themes/modern/theme';
 
-    // Any plugins you want to use has to be imported
-    import 'tinymce/plugins/advlist';
-    import 'tinymce/plugins/wordcount';
-    import 'tinymce/plugins/autolink';
-    import 'tinymce/plugins/autosave';
-    import 'tinymce/plugins/charmap';
-    import 'tinymce/plugins/codesample';
-    import 'tinymce/plugins/contextmenu';
-    import 'tinymce/plugins/emoticons';
-    import 'tinymce/plugins/codesample';
-    import 'tinymce/plugins/fullscreen';
-    import 'tinymce/plugins/hr';
-    import 'tinymce/plugins/imagetools';
-    import 'tinymce/plugins/insertdatetime';
-    import 'tinymce/plugins/link';
-    import 'tinymce/plugins/media';
-    import 'tinymce/plugins/noneditable';
-    import 'tinymce/plugins/paste';
-    import 'tinymce/plugins/print';
-    import 'tinymce/plugins/searchreplace';
-    import 'tinymce/plugins/tabfocus';
-    import 'tinymce/plugins/template';
-    import 'tinymce/plugins/textpattern';
-    import 'tinymce/plugins/visualblocks';
-    import 'tinymce/plugins/anchor';
-    import 'tinymce/plugins/autoresize';
-    import 'tinymce/plugins/bbcode';
-    import 'tinymce/plugins/code';
-    import 'tinymce/plugins/colorpicker';
-    import 'tinymce/plugins/directionality';
-    import 'tinymce/plugins/fullpage';
-    import 'tinymce/plugins/help';
-    import 'tinymce/plugins/image';
-    import 'tinymce/plugins/importcss';
-    import 'tinymce/plugins/legacyoutput';
-    import 'tinymce/plugins/lists';
-    import 'tinymce/plugins/nonbreaking';
-    import 'tinymce/plugins/pagebreak';
-    import 'tinymce/plugins/preview';
-    import 'tinymce/plugins/save';
-    import 'tinymce/plugins/spellchecker';
-    import 'tinymce/plugins/table';
-    import 'tinymce/plugins/textcolor';
-    import 'tinymce/plugins/toc';
-    import 'tinymce/plugins/visualchars';
-    
-    import 'tinymce/skins/lightgray/skin.min.css'
-    import 'tinymce/skins/lightgray/content.min.css'
-   
-    export default {
-        name: 'tinymce',
-        props: { 
-                'id' : {
-                    type : String,
-                    required : true
-                },
-                'htmlClass' : { default : '', type : String},
-                'value' : { default : '' },
-                'plugins' : { default : function(){ 
-                                    return [
-                                        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-                                        'searchreplace wordcount visualblocks visualchars code fullscreen',
-                                        'insertdatetime media nonbreaking save table contextmenu directionality',
-                                        'template paste textcolor colorpicker textpattern imagetools toc help emoticons hr'
-                                    ];
-                                } , type: Array
-                            },
-                toolbar1: { default :'formatselect | bold italic  strikethrough  forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat', type: String},
-                toolbar2: { default : '', type: String },
-                other_options: { default : function() { return {}; }, type: Object}
+// 想要使用的插件需要在这里应用
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/autosave';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/contextmenu';
+import 'tinymce/plugins/emoticons';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/hr';
+import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/noneditable';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/print';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/tabfocus';
+import 'tinymce/plugins/template';
+import 'tinymce/plugins/textpattern';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/anchor';
+import 'tinymce/plugins/autoresize';
+import 'tinymce/plugins/bbcode';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/colorpicker';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/fullpage';
+import 'tinymce/plugins/help';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/importcss';
+import 'tinymce/plugins/legacyoutput';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/nonbreaking';
+import 'tinymce/plugins/pagebreak';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/spellchecker';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/textcolor';
+import 'tinymce/plugins/toc';
+import 'tinymce/plugins/visualchars';
+
+import 'tinymce/skins/lightgray/skin.min.css'
+import 'tinymce/skins/lightgray/content.min.css'
+import 'tinymce/skins/lightgray/fonts/tinymce.woff'
+
+// 汉化包
+import '../assets/langs/zh_CN'
+
+// 导入图片管理和视频管理组件(自定义vue组件, 使用原tinymce组件的样式,重新定义的功能)
+import ImageManager from './ImageManager'
+import VideoManager from './VideoManager'
+
+export default {
+    name: 'tinymce',
+    props: {
+        'htmlClass': { default: '', type: String },
+        'value': { default: '' },
+        'plugins': {
+            default: function() {
+                return [
+                    'advlist autolink lists link image charmap print preview hr anchor pagebreak ' +
+                    'searchreplace wordcount visualblocks visualchars code fullscreen ' +
+                    'insertdatetime media nonbreaking save table contextmenu directionality ' +
+                    'template paste textcolor colorpicker textpattern imagetools toc help emoticons '
+                ];
+            }, type: Array
         },
-        data(){
-            return {
-                content : '',
-                editor : null,
-                cTinyMce : null,
-                checkerTimeout: null,
-                isTyping : false
-            }; 
+        toolbar1: { default: 'fullscreen code preview | formatselect removeformat | bold italic underline strikethrough forecolor backcolor fontselect fontsizeselect | anchor charmap link unlink | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | table insertImage insertVideo', type: String },
+        toolbar2: { default: '', type: String },
+        other_options: {
+            default: function() {
+                return {
+                    font_formats: 'Verdana=Verdana;微软雅黑=Microsoft YaHei;宋体=SimSun;黑体=SimHei;仿宋=FangSong;楷体=kaiTi;隶书=LiSu;幼圆=YouYuan;',
+                };
+            }, type: Object
         },
-        mounted(){
-            this.content = this.value;
-            this.init();  
-        },
-        beforeDestroy () {
-            this.editor.destroy();
-        },
-        watch: {
-            value : function (newValue){
-                if(!this.isTyping){
-                    if(this.editor !== null)
-                        this.editor.setContent(newValue);
-                    else
-                        this.content = newValue;
+        uploadImage: { type: Function, default: null }
+    },
+    data() {
+        return {
+            randomID: `ej_editor_container_${Math.random() * 9999999999999999}`,
+            editorID: `ej_editor_${Math.random() * 99999999999999999}`,
+            content: '',
+            editor: null,
+            cTinyMce: null,
+            checkerTimeout: null,
+            isTyping: false
+        };
+    },
+    components: {
+        ImageManager,
+        VideoManager
+    },
+    mounted() {
+        this.content = this.value;
+        this.init();
+    },
+    beforeDestroy() {
+        if(this.editor) this.editor.destroy();
+    },
+    watch: {
+        value: function(newValue) {
+            if (!this.isTyping) {
+                if (this.editor !== null) {
+                    this.editor.setContent(newValue)
+                } else {
+                    this.content = newValue
                 }
             }
         },
-        methods: {
-            init(){
-                let options = {
-                    selector: '#' + this.id,
-                    skin: false,
-                    toolbar1: this.toolbar1,
-                    toolbar2: this.toolbar2,
-                    plugins: this.plugins,
-                    init_instance_callback : (editor) => {
-                        this.editor = editor;
-                        editor.on('KeyUp', (e) => {
-                           this.submitNewContent();
-                        });
-                        editor.on('Change', (e) => {
-                            if(this.editor.getContent() !== this.value){
-                               this.submitNewContent();
-                            }
-                        });
-                        editor.on('init', (e) => {
-                            editor.setContent(this.content);
-                            this.$emit('input', this.content);
-                        });
-                    }
-                };
-                tinymce.init(this.concatAssciativeArrays(options, this.other_options));
-            },
-            concatAssciativeArrays(array1, array2){
-                if(array2.length === 0) return array1;
-                if(array1.length === 0) return array2;
-                let dest = [];
-                for ( let key in array1) dest[key] = array1[key];
-                for ( let key in array2) dest[key] = array2[key];
-                return dest;
-            },
-            submitNewContent(){
-                this.isTyping = true;
-                if(this.checkerTimeout !== null)
-                    clearTimeout(this.checkerTimeout);
-                    this.checkerTimeout = setTimeout(()=>{
-                        this.isTyping = false;
-                    }, 300);
+        // content: function(val) {
+        //     if (this.editor !== null) {
+        //         this.editor.setContent(val)
+        //     }
+        // }
+    },
+    methods: {
+        init() {
+            const editorHeight = document.getElementById(this.randomID).offsetHeight
+            console.log(this.editorID, this.randomID)
+            const that = this
+            let options = {
+                selector: `#${this.editorID}`,
+                skin: false,
+                branding: false,
+                toolbar1: this.toolbar1,
+                toolbar2: this.toolbar2,
+                menubar: false,
+                plugins: this.plugins,
+                height: editorHeight,
+                init_instance_callback: (editor) => {
+                    this.editor = editor;
+                    editor.on('KeyUp', (e) => {
+                        this.submitNewContent();
+                    });
+                    editor.on('Change', (e) => {
+                        if (this.editor.getContent() !== this.value) {
+                            this.submitNewContent();
+                        }
+                    });
+                    editor.on('init', (e) => {
+                        editor.setContent(this.content);
+                        this.$emit('init', this.content);
+                    });
+                },
+                setup(editor) {
+                    editor.addButton('insertImage', {
+                        tooltip: '插入图片',
+                        icon: 'image',
+                        onclick: () => {
+                            that.$refs.imageManager.getImage(
+                                (img) => {
+                                    editor.insertContent(`<img src="${img.url}" alt="${img.alt}" width="${img.width}" height="${img.height}" />`)
+                                }
+                            )
+                        }
+                    })
+                    editor.addButton('insertVideo', {
+                        tooltip: '插入视频',
+                        icon: 'media',
+                        onclick: () => {
+                            that.$refs.videoManager.getVideo((tagStr)=>{
+                                editor.insertContent(tagStr)
+                            })
+                        }
+                    })
+                },
+            };
+            tinymce.init(this.concatAssciativeArrays(options, this.other_options));
+        },
+        concatAssciativeArrays(array1, array2) {
+            if (array2.length === 0) return array1;
+            if (array1.length === 0) return array2;
+            let dest = [];
+            for (let key in array1) dest[key] = array1[key];
+            for (let key in array2) dest[key] = array2[key];
+            return dest;
+        },
+        submitNewContent() {
+            this.isTyping = true;
+            if (this.checkerTimeout !== null)
+                clearTimeout(this.checkerTimeout);
+            this.checkerTimeout = setTimeout(() => {
+                this.isTyping = false;
+            }, 300);
 
-                this.$emit('input', this.editor.getContent());
-            }
+            this.$emit('input', this.editor.getContent());
         }
     }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+.mce-tinymce {
+    /* height: 100%; */
+}
+.mce-floatpanel[aria-label=Preview] {
+    width: 90%;
+    left: 5% !important;
+    /* left: 0 !important; */
+}
 </style>
